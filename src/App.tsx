926c284eea5d61307d53da05f64c0c7a8c8596a5
Andrey Belyun <andreibelyun@gmail.com>
2024-06-14 18:37:25 +0300

    feat: add first pages using code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import "./index.scss";
-import { Counter } from "./components/Counter/Counter";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { HomePageLazy } from "./pages/HomePage/HomePage.lazy";
+import { Suspense } from "react";
 
 const App = () => {
   return (
     <div className="app">
-      <h1>Hello</h1>
-      <Counter />
+      <header>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+      </header>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageLazy />} />
+          <Route path={"/"} element={<HomePageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
 import App from "./App";
 import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-render(<App />, document.getElementById("root"));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
