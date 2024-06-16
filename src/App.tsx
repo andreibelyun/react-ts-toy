bbc47174074ebc48fffa34431c9cc9b7148b97ae
Andrey Belyun <andreibelyun@gmail.com>
2024-06-16 15:49:44 +0300

    feat: create context for getting and managing the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import "./index.scss";
-import { Route, Routes } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
-import { HomePageLazy } from "./pages/HomePage/HomePage.lazy";
+import "./styles/index.scss";
 import { Suspense } from "react";
+import { Link } from "react-router-dom";
+import { useTheme } from "./theme/useTheme";
+import { Route, Routes } from "react-router-dom";
+import { HomePageLazy } from "./pages/HomePage/HomePage.lazy";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <header>
+        <button onClick={toggleTheme}>Toggle theme</button>
+        <br />
         <Link to={"/"}>Home</Link>
         <Link to={"/about"}>About</Link>
       </header>
