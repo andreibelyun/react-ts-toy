dca550f49d89ad8f7985b3660cc4f4d904784866
Andrey Belyun <andreibelyun@gmail.com>
2024-07-12 12:05:17 +0300

    fix: remove unnecessary fallback

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,22 @@
 import 'app/styles/index.scss';
 
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { NavBar } from 'widgets/NavBar';
 import { AppRouter } from 'app/providers/router';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { SideBar } from 'widgets/Sidebar';
-import { PageLoader } from 'widgets/PageLoader';
 
 const App = () => {
   const { theme } = useTheme();
 
+  useEffect(() => {
+    if (Math.random() < 0.5) throw new Error();
+  }, []);
+
   return (
     <div className={classNames('app', [theme])}>
-      <Suspense fallback={<PageLoader />}>
+      <Suspense fallback="">
         <NavBar />
 
         <div className="pageContent">
